90f6ee2c99e83c275110e4085ce8f7a81b50f8a3
ThemistocIes <captainjackvorobey1@gmail.com>
2021-04-29 23:52:16 +0700

    Initial commit

---
+++ src/Tree.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tree {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = bufferedReader.readLine();
+
+    }
+}
